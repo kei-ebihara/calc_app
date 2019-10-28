dabd930a966185d8cc8b9988f3eb7b42460127e0
kei-ebihara <kei.ebihara.hikarituushin@gmail.com>
2019-10-29 01:46:29 +0900

    計算アプリを海老原ケイさんが完成させた

--- app/controllers/calcs_controller.rb
+++ app/controllers/calcs_controller.rb
@@ -1,28 +1,49 @@
+# class CalcsController < ApplicationController
+
+#   def addition
+#     @value1 = params[:value1]
+#     @value2 = params[:value2]
+#     @value = @value1.to_i + @value2.to_i
+#   end
+
+#   def subtraction
+#     @value1 = params[:value1]
+#     @value2 = params[:value2]
+#     @value = @value1.to_i - @value2.to_i
+#   end
+
+#   def multiplication
+#     @value1 = params[:value1]
+#     @value2 = params[:value2]
+#     @value = @value1.to_i * @value2.to_i
+#   end
+
+#   def division
+#     @value1 = params[:value1]
+#     @value2 = params[:value2]
+#     @value = @value1.to_i / @value2.to_i
+#   end
+
+
+# end
+
+
 class CalcsController < ApplicationController
 
   def show
     @value1 = params[:value1]
     @value2 = params[:value2]
+    @calc = params[:calc]
+    case @calc 
+    when "addition" then
     @value = @value1.to_i + @value2.to_i
-  end
-
-  def show2
-    @value1 = params[:value1]
-    @value2 = params[:value2]
+    when "subtraction" then
     @value = @value1.to_i - @value2.to_i
-  end
-
-  def show3
-    @value1 = params[:value1]
-    @value2 = params[:value2]
+    when "multiplication" then
     @value = @value1.to_i * @value2.to_i
-  end
-
-  def show4
-    @value1 = params[:value1]
-    @value2 = params[:value2]
+    when "division" then
     @value = @value1.to_i / @value2.to_i
+    end
   end
-
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
+# Rails.application.routes.draw do
+#   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+#   get 'calcs/:value1/addition/:value2' => 'calcs#addition'
+#   get 'calcs/:value1/subtraction/:value2' => 'calcs#subtraction'
+#   get 'calcs/:value1/multiplication/:value2' => 'calcs#multiplication'
+#   get 'calcs/:value1/division/:value2' => 'calcs#division'
+# end
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'calcs/:value1/addition/:value2' => 'calcs#show'
-  get 'calcs/:value1/subtraction/:value2' => 'calcs#show2'
-  get 'calcs/:value1/multiplication/:value2' => 'calcs#show3'
-  get 'calcs/:value1/division/:value2' => 'calcs#show4'
+  get 'calcs/:value1/:calc/:value2' => 'calcs#show'
 end
